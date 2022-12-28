c985c1efb7da3dd657f008b4c204166c5e39acb5
alexingarojas <alezingaa@gmail.com>
2022-12-28 09:49:55 -0500

    Db and Context

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration=configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+    }
+}
+
---
+++ Infrastucture/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace Infrastucture.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        { }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        { 
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+    }
+}
